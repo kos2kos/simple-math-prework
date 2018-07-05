d70431dd3b10d868a70f20e1a5d8d18205c3ea8a
Nkosi Enciso Givhan <koasterman@gmail.com>
2018-07-05 12:39:08 +0000

    automatically backed up by learn

--- lib/math.rb
+++ lib/math.rb
@@ -24,5 +24,5 @@ end
 
 def order_of_operation(num1, num2, num3, num4)
   #Hint:  __ + (( __ * __ ) / __ )
-  division(multiplication(num2,num3)
+  addition(num1, division(multiplication(num2,num3),num4))
 end
